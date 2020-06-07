eb0fcb05a8ec25ae5e4b067b715e5ecdd6bc0f72
Timothy Klenke <timothy.klenke@gmail.com>
2020-06-07 00:12:59 -0600

    Winner detection

--- TicTacToe/TicTacToe.py
+++ TicTacToe/TicTacToe.py
@@ -1,15 +1,21 @@
 board = [str(x) for x in list(range(1,10))]
 
+def row(x):
+    return board[(x-1)*3:x*3]
+def col(x):
+    return board[x-1:9:3]
+def forward_cross_line():
+    return [board[0]] + [board[4]] + [board[8]]
+def back_cross_line():
+    return [board[2]] + [board[4]] + [board[6]]
+
 def print_board():
-    print(board[6:9])
-    print(board[3:6])
-    print(board[0:3])
+    print(row(3))
+    print(row(2))
+    print(row(1))
 
 def available_inputs():
     return [x for x in board if x.isdigit()]
-
-def is_game_over():
-    return available_inputs() == []
 
 def get_input(player):
     available = available_inputs()
@@ -20,6 +26,38 @@ def get_input(player):
         else:
             print("The available selections are: {}".format(' '.join(available)))
 
+
+
+def winner():
+    def lines():
+        return[row(1), row(2), row(3), col(1), col(2), col(3), forward_cross_line(), back_cross_line()]
+
+    def line_winner(line):
+        token = set(line)        
+        if len(token)==1:
+           return token.pop()
+        else:
+           return False
+
+    for line in lines():        
+        if line_winner(line):
+            return line_winner(line)
+        
+    return False
+
+def is_game_over():
+    return available_inputs() == [] or winner() != False
+
+def print_game_result():
+    print()
+    winner = winner()
+    if winner == False:
+        print("The game is a draw")
+    else:
+        print("The winner is player {}".format(winner))
+    print_board()
+
+#GAME MAIN
 player = 'X'
 
 while not is_game_over():
@@ -27,4 +65,6 @@ while not is_game_over():
     cell = get_input(player)
     board[cell] = player
     player = 'O' if player=='X' else 'X'
+
+print_game_result()
 
