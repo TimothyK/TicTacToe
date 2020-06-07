a1c46783133d7e6ef6f224118bf39253863eac75
Timothy Klenke <timothy.klenke@gmail.com>
2020-06-07 15:51:01 -0600

    Make Board properties private

--- TicTacToe/Board.py
+++ TicTacToe/Board.py
@@ -1,35 +1,34 @@
 class Board:
     def __init__(self):
-        self.board = [str(x) for x in list(range(1,10))]
+        self.__board = [str(x) for x in list(range(1,10))]
 
-    def row(self, x):
-        return self.board[(x-1)*3:x*3]
-    def col(self, x):
-        return self.board[x-1:9:3]
-    def forward_cross_line(self):
-        return [self.board[0]] + [self.board[4]] + [self.board[8]]
-    def back_cross_line(self):
-        return [self.board[2]] + [self.board[4]] + [self.board[6]]
-
+    def __row(self, x):
+        return self.__board[(x-1)*3:x*3]
+    def __col(self, x):
+        return self.__board[x-1:9:3]
+    def __forward_cross_line(self):
+        return [self.__board[0]] + [self.__board[4]] + [self.__board[8]]
+    def __back_cross_line(self):
+        return [self.__board[2]] + [self.__board[4]] + [self.__board[6]]
+    def __lines(self):
+        return[self.__row(1), self.__row(2), self.__row(3), self.__col(1), self.__col(2), self.__col(3), self.__forward_cross_line(), self.__back_cross_line()]
+    
     def print_board(self):
-        print(self.row(3))
-        print(self.row(2))
-        print(self.row(1))
+        print(self.__row(3))
+        print(self.__row(2))
+        print(self.__row(1))
 
     def available_inputs(self):
-        return [x for x in self.board if x.isdigit()]
+        return [x for x in self.__board if x.isdigit()]
 
     def change_cell(self, cell, player_mark):
         '''
         cell should be a value returned from available_inputs
         '''
         pos = int(cell)-1
-        self.board[pos] = player_mark
+        self.__board[pos] = player_mark
 
     def winner(self):
-        def lines():
-            return[self.row(1), self.row(2), self.row(3), self.col(1), self.col(2), self.col(3), self.forward_cross_line(), self.back_cross_line()]
-
         def line_winner(line):
             token = set(line)        
             if len(token)==1:
@@ -37,7 +36,7 @@ class Board:
             else:
                return False
 
-        for line in lines():        
+        for line in self.__lines():        
             if line_winner(line):
                 return line_winner(line)
         
